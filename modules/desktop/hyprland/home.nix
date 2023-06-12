e27cf7cf269b49a7995b6899afc59a4f3e82455e
spruce <tungtose@gmail.com>
2023-06-13 01:45:14 +0700

    remove: redundant bind

--- modules/desktop/hyprland/home.nix
+++ modules/desktop/hyprland/home.nix
@@ -257,8 +257,6 @@
       # quickly launch program #
       #------------------------# 
       bind=$mainMod,B,exec,firefox
-      bind=$mainMod,M,exec,netease-cloud-music-gtk4 
-      bind=$mainMod SHIFT,M,exec,kitty --class="musicfox" --hold sh -c "musicfox" 
       bind=$mainMod SHIFT,D,exec,kitty  --class="danmufloat" --hold sh -c "export TERM=xterm-256color && bili"
       bind=$mainMod SHIFT,X,exec,myswaylock
       bind=$mainMod,bracketleft,exec,grimblast --notify --cursor  copysave area ~/Pictures/$(date "+%Y-%m-%d"T"%H:%M:%S_no_watermark").png

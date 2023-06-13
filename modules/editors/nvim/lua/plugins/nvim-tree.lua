5540b9de2fe03ba5ac1b501a84ae2acb6be6bbf5
spruce <tungtose@gmail.com>
2023-06-13 17:32:01 +0700

    update: new api nvim-tree adapted

--- modules/editors/nvim/lua/plugins/nvim-tree.lua
+++ modules/editors/nvim/lua/plugins/nvim-tree.lua
@@ -1,29 +1,35 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
+	vim.keymap.set("n", "<C-n>", ":NvimTreeOpen<CR>")
+	vim.keymap.set("n", "<C-f>", ":NvimTreeFindFile<CR>")
+	vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
+end
+
 return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "tt", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree" },
-	},
 	config = function()
-		vim.keymap.set("n", "<leader>n", ":NvimTreeForcus<CR>")
-		vim.keymap.set("n", "<C-n>", ":NvimTreeOpen<CR>")
-		vim.keymap.set("n", "<C-f>", ":NvimTreeFindFile<CR>")
-		vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
-
-		require("nvim-tree").setup({ -- BEGIN_DEFAULT_OPTS
+		require("nvim-tree").setup({
+			on_attach = on_attach,
 			sort_by = "case_sensitive",
 			renderer = {
 				group_empty = true,
 			},
 			view = {
 				adaptive_size = true,
-				mappings = {
-					list = {
-						{ key = "u", action = "dir_up" },
-						{ key = "s", action = "" },
-						{ key = "o", action = "system_open" },
-					},
-				},
 				side = "left",
 			},
 		})

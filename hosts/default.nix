0b47f5b29add2bd7e358cb123b7ea44c8241c306
spruce <tungtose@gmail.com>
2023-07-09 17:24:53 +0700

    update: rebase x11 main

--- hosts/default.nix
+++ hosts/default.nix
@@ -14,8 +14,8 @@ in
     inherit system;
     specialArgs = { inherit inputs user; };
     modules = [
-      # ./laptop/wayland
-      ./laptop/x11
+      ./laptop/wayland
+      # ./laptop/x11
     ] ++ [
       ./system.nix
     ] ++ [
@@ -28,8 +28,8 @@ in
           extraSpecialArgs = { inherit user; };
           users.${user} = {
             imports = [
-              # (import ./laptop/wayland/home.nix)
-              (import ./laptop/x11/home.nix)
+              (import ./laptop/wayland/home.nix)
+              # (import ./laptop/x11/home.nix)
             ] ++ [
               inputs.hyprland.homeManagerModules.default
             ];

e393eb8c56053dc9c11bc32862966f5239165945
spruce <tungtose@gmail.com>
2023-10-11 23:47:57 +0700

    update: vim like helix

--- modules/editors/helix/default.nix
+++ modules/editors/helix/default.nix
@@ -3,5 +3,5 @@
 {
   home.file.".config/helix/config.toml".source = ./config.toml;
   home.file.".config/helix/languages.toml".source = ./languages.toml;
-
+  home.file.".config/helix/themes/od.toml".source = ./od.toml;
 }
